6d585399a1bf81822ac37f465d96297f695bf114
Tellurium <49403874+TelluriumCoin@users.noreply.github.com>
2019-04-14 19:03:29 +0300

    Add files via upload

---
+++ src/config/Ascii.h
@@ -0,0 +1,11 @@
+// Copyright (c) 2018, The TurtleCoin Developers
+// Copyright (c) 2018, The DeroGold Association
+//
+// Please see the included LICENSE file for more information
+
+#pragma once
+
+const std::string asciiArt =
+"\n"
+"Copyright (c) 2019 - Tellurium \n"
+"The future is now\n";
