ef65d9b7a3ed6effe08068e4853dfcea02d49772
Tellurium <49403874+TelluriumCoin@users.noreply.github.com>
2019-04-15 20:50:15 +0300

    Update CryptoNoteConfig.h

--- src/config/CryptoNoteConfig.h
+++ src/config/CryptoNoteConfig.h
@@ -1,10 +1,3 @@
-// Copyright (c) 2012-2017, The CryptoNote developers, The Bytecoin developers
-// Copyright (c) 2014-2018, The Monero Project
-// Copyright (c) 2018-2019, The TurtleCoin Developers
-// Copyright (c) 2018-2019, The DeroGold Assosiation
-// Copyright (c) 2018-2019, The BitcoinMono Developers
-// Copyright (c) 2019-2019, The Tellurium Developers
-// Please see the included LICENSE file for more information
 
 #pragma once
 
@@ -240,3 +233,12 @@ const char* const SEED_NODES[] = {
 
 };
 } // CryptoNote
+
+// Copyright (c) 2012-2017, The CryptoNote developers, The Bytecoin developers
+// Copyright (c) 2014-2018, The Monero Project
+// Copyright (c) 2018-2019, The TurtleCoin Developers
+// Copyright (c) 2018-2019, The DeroGold Assosiation
+// Copyright (c) 2018-2019, The BitcoinMono Developers
+// Copyright (c) 2019-2019, The Tellurium Developers
+// Please see the included LICENSE file for more information.
+
