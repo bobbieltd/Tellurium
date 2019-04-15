73d65c0acd4acc1a7843ff4958cd5cf927aa4234
Tellurium <49403874+TelluriumCoin@users.noreply.github.com>
2019-04-15 21:13:40 +0300

    Code Cleaning
    DONE

--- include/CryptoNote.h
+++ include/CryptoNote.h
@@ -14,6 +14,15 @@
 //
 // You should have received a copy of the GNU Lesser General Public License
 // along with Bytecoin.  If not, see <http://www.gnu.org/licenses/>.
+//
+// Copyright (c) 2012-2017, The CryptoNote developers, The Bytecoin developers
+// Copyright (c) 2014-2018, The Monero Project
+// Copyright (c) 2018-2019, The TurtleCoin Developers
+// Copyright (c) 2018-2019, The DeroGold Assosiation
+// Copyright (c) 2018-2019, The BitcoinMono Developers
+// Copyright (c) 2019-2019, The Tellurium Developers
+// Please see the included LICENSE file for more information.
+
 
 #pragma once
 
